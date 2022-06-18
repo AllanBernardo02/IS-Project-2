1cb005ba895f55033aa697fdca1ee3d1169c3227
Allan02 <85282679+AllanBernardo02@users.noreply.github.com>
2022-06-18 21:32:31 +0800

    fix navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -53,7 +53,7 @@ const Navbar = () => {
             <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
             <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
-            <Button variant="contained" className={classes.back} color="secondary" onClick={back}>back to StudentModule</Button>
+            {/*<Button variant="contained" className={classes.back} color="secondary" onClick={back}>back to StudentModule</Button>*/}
           </div>
         ) : (
           <React.Fragment>
