83b4b5de527c05c47307f88ac2cf0f5bce4bd2f0
Allan02 <85282679+AllanBernardo02@users.noreply.github.com>
2022-06-27 09:59:30 +0800

    final commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
@@ -16,6 +17,7 @@ import cafaRoutes from './routes/cafaPosts.js'
 import claRoutes from './routes/claPosts.js'
 
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -33,16 +35,16 @@ app.use('/cafaposts', cafaRoutes)
 app.use('/claposts', claRoutes)
 
 app.get('/', (req, res) => {
-  res.send('APP IS RUNNING')
+  res.send('APP IS RUNNING') 
 })
 // original db 
 // const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
 
-const CONNECTION_URL = 'mongodb+srv://codewithallan:toshiro02@cluster0.sov3f.mongodb.net/?retryWrites=true&w=majority'; 
+// const CONNECTION_URL = 'mongodb+srv://codewithallan:toshiro02@cluster0.sov3f.mongodb.net/?retryWrites=true&w=majority'; 
    
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
